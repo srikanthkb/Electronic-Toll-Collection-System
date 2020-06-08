f12c5a86a6507972eafd262036ee93146a44a40f
Srikanth K B <srikanth.kadaba@gmail.com>
2020-06-08 20:24:12 +0530

    Create wimSensorSystem.py
    Just a simulation of a WIM Sensor.
    Doesn't operate.
    Integration with other components made easy.

---
+++ wimSensorSystem.py
@@ -0,0 +1,11 @@
+class WimSensorSystem:
+    ''' This measures the weight based on weigh-in-motion sensors.
+        Input: WIM sensor reading.
+        Output: Weight in kgs
+    '''
+    def __init__(self):
+        pass
+    
+    def getWeight(self):
+        ''' Return sample weight for now'''
+        return 150
